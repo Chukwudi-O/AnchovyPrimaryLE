73e649b3d0e8de62b1846dc7b97271aa082e61ad
Chukwudi <57363749+Chukwudi-O@users.noreply.github.com>
2020-11-29 12:59:27 -0500

    updated Index.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
-function validateLogIn(){
-	return;
+admin_b = document.getElementById("adminB");
+teacher_b = document.getElementById("teacherB");
+student_b = document.getElementById("studentB");
+
+
+
+
+function redirect(to){
+	if (to === "admin"){
+		console.log("Admin Clicked.");
+		document.location.href="adminLogin.html";
+	}else if (to === "teach"){
+		console.log("Teacher Clicked.");
+		document.location.href="teacherLogin.html";
+	}else if (to === "stud"){
+		console.log("Student Clicked.");
+		document.location.href="studentLogin.html";
+	}
 }
