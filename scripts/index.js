03951713d05f53922a5cc03e29f9ac10febe57d5
Chukwudi <57363749+Chukwudi-O@users.noreply.github.com>
2020-11-29 13:16:43 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ student_b = document.getElementById("studentB");
 function redirect(to){
 	if (to === "admin"){
 		console.log("Admin Clicked.");
-		document.location.href="adminLogin.html";
+		document.location.href="adminlogin.html";
 	}else if (to === "teach"){
 		console.log("Teacher Clicked.");
 		document.location.href="teacherLogin.html";
