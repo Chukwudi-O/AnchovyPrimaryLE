8444929519a9087703dbf82908ae6e992e3ee80b
Chukwudi <57363749+Chukwudi-O@users.noreply.github.com>
2020-11-30 21:31:55 -0500

    Add files via upload

---
+++ scripts/adminHome.js
@@ -0,0 +1,13 @@
+function selectOp(index){
+	if (index == 0){
+		console.log("Index 0");
+	}else if(index == 1){
+		console.log("Index 1");
+	}else if(index == 2){
+		console.log("Index 2");
+	}else if(index == 3){
+		console.log("Index 3");
+	}else if(index == 4){
+		console.log("Index 4");
+	} 
+}
